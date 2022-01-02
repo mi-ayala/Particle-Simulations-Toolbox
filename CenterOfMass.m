6ff37220afcb8f7f6eb6e3c02c9d318bad98f6cf
Miguel Ayala <miguel.guadarramaayala@mail.mcgill.ca>
2022-01-01 19:17:00 -0500

    Functions

--- CenterOfMass.m
+++ CenterOfMass.m
@@ -1,1 +1,10 @@
 
+function X0 = CenterOfMass(X0)
+
+    N = length(X0(:,1));
+
+    CMass = (1/N).*sum(X0, 1);
+
+    X0 = X0 - CMass;
+
+end
--- DistanceToCM.m
+++ DistanceToCM.m
@@ -1,1 +1,7 @@
+function bin = DistanceToCM(X0, n)
+    
+    %%% Distances to the origin
+    distance = sqrt(sum(X0.^2,2));
+    [~,~,bin] = histcounts(distance,n);
 
+end
